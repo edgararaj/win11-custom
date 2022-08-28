305b1ee2533f1f5717a22a9fffd105c40a1a013d
Edgar Araújo <edgararaj@gmail.com>
2022-08-29 00:38:10 +0100

    init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+# Place in: %USERPROFILE%\Documents\WindowsPowerShell\
+Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Enter-VsDevShell 7056a96e -SkipAutomaticLocation -DevCmdArguments '-arch=x64 -no_logo'
